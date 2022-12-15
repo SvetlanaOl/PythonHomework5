0f574f0ecb437349c75256d1d7ea6e28d0706003
Svetlana M <jygik91@gmail.com>
2022-12-15 15:53:04 +0700

    Решение задачи 1

--- Task1.py
+++ Task1.py
@@ -1,1 +1,8 @@
-
+print()
+print('1.Задача: Удалить из текста все слова, содержащие ""абв""')
+with open('text.txt', 'r', encoding = 'utf_8') as data:
+    s = data.read().split()
+print(f'В файле записано: {s}')
+print('Удалили все слова с абв и получили: ')
+print(' '.join([word for word in s if 'абв' not in word]))
+print()
